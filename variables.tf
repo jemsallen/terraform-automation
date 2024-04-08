a5be8a46b76b48f0ccf8491f7c0f8eb30e4e8473
Jemma Allen <jems.allen@gmail.com>
2024-04-08 11:08:30 +0100

    Add demo

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "demo-bucket-name" {
+  type = string
+  description = "Bucket name"
+}
+
+variable "environment" {
+  type = string
+  description = "Environment name"
+}
+
+variable "bucket_config" {
+  type = map(string)
+  description = "Bucket configuration"
+}
